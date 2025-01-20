2eb4a1efd9f7d0885482c77207c8a574e61cfab0
Kevin Barnes <kbarnes3@gmail.com>
2025-01-19 22:09:43 -0800

    Adding mandatory registry changes to script

--- Add-GPUToVM.ps1
+++ Add-GPUToVM.ps1
@@ -3,6 +3,13 @@ param(
 [string]$VMName
 )
 
+if (-Not (Test-Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\HyperV)) {
+    New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\HyperV
+}
+
+Set-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\HyperV -Name "RequireSecureDeviceAssignment" -Type DWORD -Value 0
+Set-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\HyperV -Name "RequireSupportedDeviceAssignment" -Type DWORD -Value 0
+
 if (Get-VMGpuPartitionAdapter -VMName $VMName -ErrorAction SilentlyContinue) {
    Remove-VMGpuPartitionAdapter -VMName $VMName
 }
@@ -10,3 +17,5 @@ Set-VM -GuestControlledCacheTypes $true -VMName $VMName
 Set-VM -LowMemoryMappedIoSpace 1Gb -VMName $VMName
 Set-VM -HighMemoryMappedIoSpace 32Gb -VMName $VMName
 Add-VMGpuPartitionAdapter -VMName $VMName
+
+Write-Host "GPU-PV configured for $VMName"
